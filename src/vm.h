c120a8d62efa1f4c4c008fbc4aafad171b2d085e
Soukarya04 <90480806+Soukarya04@users.noreply.github.com>
2026-06-03 19:50:30 +0530

    Update vm.h

--- src/vm.h
+++ src/vm.h
@@ -2,33 +2,49 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <compiler.h>
+#include <unordered_map>
 
-enum class ValType { INT, BOOL };
+
+enum class ValType { INT, BOOL, STRING };
 struct Value {
     ValType type = ValType::INT;
-    union { int32_t i = 0; bool b; };
+    int32_t i = 0;      
+    bool b = false;     
+    std::string s;      
 
     static Value fromInt (int32_t v) { Value val; val.type = ValType::INT;  val.i = v; return val; }
-    static Value fromBool(bool    v) { Value val; val.type = ValType::BOOL; val.b = v; return val; }
+    static Value fromBool(bool v) { Value val; val.type = ValType::BOOL; val.b = v; return val; }
+    static Value fromString(std::string v) { Value val; val.type = ValType::STRING; val.s = v; return val; }
 
-    // in cvm++, any non-zero int or true is truthy
+    // in cvm++, any "non-zero int" or "true" or "non-empty string" is truthy
     bool truthy() const {
-        return (type == ValType::BOOL) ? b : (i != 0);
+        if (type == ValType::BOOL)   return b;
+        if (type == ValType::STRING) return !s.empty(); 
+        return i != 0;
     }
 
     std::string toString() const {
-        if (type == ValType::BOOL) return b ? "true" : "false";
+        if (type == ValType::BOOL)   return b ? "true" : "false";
+        if (type == ValType::STRING) return s;         
         return std::to_string(i);
     }
 };
 
+struct CallFrame {
+    const Chunk* chunk;          // which chunk
+    size_t ip;                   // instruction ptr
+    size_t stackBase;            // where frame's locals start on the stack
+    std::vector<Value> vars;     // frame's variable slots
+};
+
 class VM {
 public:
-    void run(const std::vector<uint8_t>& code);
+    void run(const Chunk& chunk, const std::unordered_map<std::string, FunctionObject>& functions);
 
 private:
     std::vector<Value> stack;
-    std::vector<Value> vars;   // 256 variable slots
+    std::vector<CallFrame> frames;
 
     void  push(Value v);
     Value pop();
