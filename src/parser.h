60f1ed84a2444729f9f998348f2e58d5b99d645d
Soukarya04 <90480806+Soukarya04@users.noreply.github.com>
2026-06-03 19:49:21 +0530

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -23,6 +23,7 @@ private:
     std::unique_ptr<Stmt> parseStmt();
     std::unique_ptr<Stmt> parseLetStmt();
     std::unique_ptr<Stmt> parseIfStmt();
+    std::unique_ptr<Stmt> parseForStmt();
     std::unique_ptr<Stmt> parseWhileStmt();
     std::unique_ptr<Stmt> parsePrintStmt();
     StmtList parseBlock();
@@ -33,4 +34,10 @@ private:
     std::unique_ptr<Expr> parseAddition();
     std::unique_ptr<Expr> parseMultiply();
     std::unique_ptr<Expr> parsePrimary();
+
+    // function parsers
+    std::unique_ptr<Stmt> parseFnDecl();
+    std::unique_ptr<Stmt> parseReturnStmt();
+    std::unique_ptr<Expr> parseCall(std::string name);  
+    std::unique_ptr<Expr> parseLogical();  
 };
