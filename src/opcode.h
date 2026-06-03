0c8a758f587782bfe5fd145dd85644633d129355
Soukarya04 <90480806+Soukarya04@users.noreply.github.com>
2026-06-03 19:48:31 +0530

    Update opcode.h

--- src/opcode.h
+++ src/opcode.h
@@ -7,11 +7,17 @@ enum class OpCode : uint8_t {
     PUSH_FALSE,
     LOAD,
     STORE,
-    ADD, SUB, MUL, DIV,
-    EQ, LESS,
+    ADD, SUB, MUL, DIV, MOD,
+    NEG, NOT,
+    EQ, NEQ, LESS, GREATER, LESS_EQ, GREATER_EQ,
     PRINT,
     INPUT,
+    SINPUT,
     JUMP,
     JUMP_IF_FALSE,
+    PUSH_STR,
+    CALL,
+    RETURN,
     POP,
-    HALT };
+    HALT 
+};
